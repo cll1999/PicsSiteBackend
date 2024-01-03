c77eee06b5bf7a185f457cb06e992c91e00689a4
Claire <lundycl@comcast.net>
2024-01-03 14:41:27 -0400

    added pool idsA

--- lib/LoginLambda/login.ts
+++ lib/LoginLambda/login.ts
@@ -2,19 +2,19 @@ const AWS = require('aws-sdk');
 import { Handler } from "aws-cdk-lib/aws-lambda";
 const AmazonCognitoIdentity = AWS.AmazonCognitoIdentity;
 
-export const handler: Handler = async () => {
+export const handler: Handler = async (username: string, password: string) => {
   var authenticationData = {
-    Username: 'username',
-    Password: 'password',
+    Username: username,
+    Password: password,
   };
   var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
   var poolData = {
-    UserPoolId: 'us-east-1_ExaMPle',
-    ClientId: '1example23456789'
+    UserPoolId: 'us-east-1_EY9v4noSY',
+    ClientId: '7ebofnhd12iheom0s9kgg24vpn'
   };
   var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
   var userData = {
-    Username: 'username',
+    Username: username,
     Pool: userPool
   };
   var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
@@ -24,20 +24,29 @@ export const handler: Handler = async () => {
 
       /* Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer */
       var idToken = result.idToken.jwtToken;
+
+      return {
+        body: JSON.stringify({ token: accessToken }),
+        statusCode: 200,
+        isBase64Encoded: false,
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }
     },
 
     onFailure: function (err: any) {
       alert(err);
+
+      return {
+        body: JSON.stringify({ error: err }),
+        statusCode: 500,
+        isBase64Encoded: false,
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }
     },
 
   });
-
-  return {
-    body: JSON.stringify({ message: 'Hello from Lambda!' }),
-    statusCode: 200,
-    isBase64Encoded: false,
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  }
 };
